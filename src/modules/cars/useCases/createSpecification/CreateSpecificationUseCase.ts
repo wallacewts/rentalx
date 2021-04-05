ffe5f0778564b31f4cd93462de261ca632167ebe
Matheus Wallace <matheuswts2011@gmail.com>
2021-04-05 10:49:10 -0300

    add global handler error

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import AppError from "../../../../errors/AppError";
 import ISpecificationsRepository from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -20,7 +21,7 @@ export default class CreateSpecificationUseCase {
     );
 
     if (specificationAltreadyExists) {
-      throw new Error("Specification already exists!");
+      throw new AppError("Specification already exists!");
     }
 
     await this.specificationRepository.create({ description, name });
