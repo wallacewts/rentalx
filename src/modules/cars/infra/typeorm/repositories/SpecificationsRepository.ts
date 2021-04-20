010c36aa37d17ff00f0d318e1a3ad3964cf1e281
Matheus Wallace <matheuswts2011@gmail.com>
2021-04-19 21:06:52 -0300

    refactor application folders

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,9 +1,10 @@
 import { getRepository, Repository } from "typeorm";
 
-import Specification from "../../entities/Specification";
 import ISpecificationsRepository, {
   ICreateSpecificationDTO,
-} from "../ISpecificationsRepository";
+} from "@modules/cars/repositories/ISpecificationsRepository";
+
+import Specification from "../entities/Specification";
 
 export default class SpecificationsRepository
   implements ISpecificationsRepository {
