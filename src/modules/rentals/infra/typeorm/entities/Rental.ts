48cb0af17aace1611e039a4f5d992ed151684e85
Matheus Wallace <matheuswts2011@hotmail.com>
2021-05-09 14:26:57 -0300

    add create rental use case and unit tets

---
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default class Rental {
+  id: string;
+
+  car_id: string;
+
+  user_id: string;
+
+  start_date: Date;
+
+  end_date: Date;
+
+  expected_return_data: Date;
+
+  total: number;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
